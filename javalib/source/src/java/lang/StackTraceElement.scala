scala-js/scala-js

Fix #184: Add support for Throwable.printStackTrace().
Stack traces are captured when a new Throwable (or subclass)
is instantiated. JavaScriptExceptions capture their underlying
JS exception's stack trace instead of themselves. The trait
scala.util.control.NoStackTrace disables capturing the state
entirely, for efficiency, just like on the JVM.

When printStackTrace() or getStackTrace() is called (not before),
the stack traces are analyzed in a browser-dependent way, and
translated to an Array[StackTraceElement].

javalib/source/src/java/lang/StackTraceElement.scala
@@ -1,0 +1,44 @@
+package java.lang
+
+final class StackTraceElement(declaringClass: String, methodName: String,
+    fileName: String, lineNumber: Int) extends AnyRef with java.io.Serializable {
+
+  def getFileName(): String = fileName
+  def getLineNumber(): Int = lineNumber
+  def getClassName(): String = declaringClass
+  def getMethodName(): String = methodName
+  def isNativeMethod(): scala.Boolean = false
+
+  override def equals(that: Any): scala.Boolean = that match {
+    case o: StackTraceElement =>
+      (getFileName == o.getFileName) &&
+      (getLineNumber == o.getLineNumber) &&
+      (getClassName == o.getClassName) &&
+      (getMethodName == o.getMethodName)
+    case _ =>
+      false
+  }
+
+  override def toString(): String = {
+    var result = ""
+    if (declaringClass != "<jscode>")
+      result += declaringClass + "."
+    result += methodName
+    if (fileName eq null) {
+      if (isNativeMethod)
+        result += "(Native Method)"
+      else
+        result += "(Unknown Source)"
+    } else {
+      result += s"($fileName"
+      if (lineNumber >= 0)
+        result += s":$lineNumber"
+      result += ")"
+    }
+    result
+  }
+
+  override def hashCode(): Int = {
+    declaringClass.hashCode() ^ methodName.hashCode()
+  }
+}

MasseGuillaume/multi-platforms-javalibs

javalib/source/src/java/lang/StackTraceElement.scala
@@ -1,0 +1,44 @@
+package java.lang
+
+final class StackTraceElement(declaringClass: String, methodName: String,
+    fileName: String, lineNumber: Int) extends AnyRef with java.io.Serializable {
+
+  def getFileName(): String = fileName
+  def getLineNumber(): Int = lineNumber
+  def getClassName(): String = declaringClass
+  def getMethodName(): String = methodName
+  def isNativeMethod(): scala.Boolean = false
+
+  override def equals(that: Any): scala.Boolean = that match {
+    case that: StackTraceElement =>
+      (getFileName == that.getFileName) &&
+      (getLineNumber == that.getLineNumber) &&
+      (getClassName == that.getClassName) &&
+      (getMethodName == that.getMethodName)
+    case _ =>
+      false
+  }
+
+  override def toString(): String = {
+    var result = ""
+    if (declaringClass != "<jscode>")
+      result += declaringClass + "."
+    result += methodName
+    if (fileName eq null) {
+      if (isNativeMethod)
+        result += "(Native Method)"
+      else
+        result += "(Unknown Source)"
+    } else {
+      result += s"($fileName"
+      if (lineNumber >= 0)
+        result += s":$lineNumber"
+      result += ")"
+    }
+    result
+  }
+
+  override def hashCode(): Int = {
+    declaringClass.hashCode() ^ methodName.hashCode()
+  }
+}